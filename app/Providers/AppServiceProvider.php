d8a1997d6c10351364701b9ebe488678f21ecb1a
Yasser <yassergharieb14@gmail.com>
2026-04-26 22:14:46 +0300

    init project

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\BookingRepositoryInterface;
+use App\Repositories\NullBookingRepository;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @throws \ReflectionException
+     */
+    public function register(): void
+    {
+        App::bind(BookingRepositoryInterface::class, NullBookingRepository::class);
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
