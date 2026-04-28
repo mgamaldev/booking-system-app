79bebbfcc5568ab20a2a7b827b1702e3fc408845
Yasser <yassergharieb14@gmail.com>
2026-04-28 16:51:28 +0300

    add Larastan for static analysis and bind NullBookingRepository to BookingRepositoryInterface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,20 @@
 
 namespace App\Providers;
 
+use App\Repositories\BookingRepositoryInterface;
+use App\Repositories\NullBookingRepository;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     * @throws \ReflectionException
      */
     public function register(): void
     {
-        //
+        App::bind(BookingRepositoryInterface::class , NullBookingRepository::class);
     }
 
     /**
